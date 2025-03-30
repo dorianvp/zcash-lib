9ee7f5698f73eb4edac420d7a006f32a926f74ac
dorianvp <dorianvp@zingolabs.org>
2025-03-30 18:09:45 +0200

    docs: initial commit

---
+++ website/src/app/docs/layout.tsx
@@ -0,0 +1,16 @@
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import type { ReactNode } from "react";
+import { baseOptions } from "@/app/layout.config";
+import { source } from "@/lib/source";
+
+export default function Layout({ children }: { children: ReactNode }) {
+	return (
+		<DocsLayout
+			githubUrl="https://github.com/dorianvp/learning-zcash"
+			tree={source.pageTree}
+			{...baseOptions}
+		>
+			{children}
+		</DocsLayout>
+	);
+}

69a6aaf07a112400730bb52cc7c79e24993b3dbe
dorianvp <dorianvp@zingolabs.org>
2025-04-04 15:24:08 +0200

    feat(website): add vercel packages

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,18 +1,22 @@
-import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import "./global.css";
+import { RootProvider } from "fumadocs-ui/provider";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
-  subsets: ['latin'],
+	subsets: ["latin"],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={inter.className} suppressHydrationWarning>
+			<body className="flex flex-col min-h-screen">
+				<Analytics />
+				<SpeedInsights />
+				<RootProvider>{children}</RootProvider>
+			</body>
+		</html>
+	);
 }

caac78f47b3d8fc00eb581ecc6ce433eef54912b
dorianvp <dorianvp@zingolabs.org>
2025-04-08 12:42:29 +0200

    chore: add GSC code

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Head from "next/head";
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -12,6 +13,12 @@ const inter = Inter({
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<html lang="en" className={inter.className} suppressHydrationWarning>
+			<Head>
+				<meta
+					name="google-site-verification"
+					content="N0KBKuwAzMbWMp-ll7zsxnxRfSi8-DUco79Wjhi7JJI"
+				/>
+			</Head>
 			<body className="flex flex-col min-h-screen">
 				<Analytics />
 				<SpeedInsights />

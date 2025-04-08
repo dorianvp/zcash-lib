d0386349ec177e784d1168791d4699d5660a675d
dorianvp <dorianvp@zingolabs.org>
2025-04-08 12:54:26 +0200

    fix(gsc): use Next.js's metadata object

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -4,21 +4,23 @@ import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import Head from "next/head";
+import { Metadata } from "next";
 
 const inter = Inter({
 	subsets: ["latin"],
 });
 
+export const metadata: Metadata = {
+	title: "Learning Zcash",
+	description: "A Rust library for learning Zcash",
+	verification: {
+		google: "N0KBKuwAzMbWMp-ll7zsxnxRfSi8-DUco79Wjhi7JJI",
+	},
+};
+
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<html lang="en" className={inter.className} suppressHydrationWarning>
-			<Head>
-				<meta
-					name="google-site-verification"
-					content="N0KBKuwAzMbWMp-ll7zsxnxRfSi8-DUco79Wjhi7JJI"
-				/>
-			</Head>
 			<body className="flex flex-col min-h-screen">
 				<Analytics />
 				<SpeedInsights />

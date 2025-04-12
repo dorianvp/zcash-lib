b0ed2b7b7b34bf58de8a78638549ad658a24ad05
dorianvp <dorianvp@zingolabs.org>
2025-04-12 02:08:05 +0200

    feat: add favicon and app-icon.tsx

---
+++ website/src/components/icons/app-icon.tsx
@@ -0,0 +1,55 @@
+export function AppIcon() {
+	return (
+		<svg viewBox="0 0 138 138" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<rect width="138" height="138" rx="69" fill="url(#paint0_radial_2_9)" />
+			<ellipse
+				cx="105.5"
+				cy="68.5"
+				rx="23.5"
+				ry="39.5"
+				fill="url(#paint1_linear_2_9)"
+			/>
+			<ellipse
+				cx="18.5"
+				cy="68.5"
+				rx="7.5"
+				ry="17.5"
+				fill="url(#paint2_linear_2_9)"
+			/>
+			<defs>
+				<radialGradient
+					id="paint0_radial_2_9"
+					cx="0"
+					cy="0"
+					r="1"
+					gradientUnits="userSpaceOnUse"
+					gradientTransform="translate(35 72) rotate(-24.7024) scale(137.591)"
+				>
+					<stop stopColor="#F4B728" />
+					<stop offset="1" stopColor="#FFD062" />
+				</radialGradient>
+				<linearGradient
+					id="paint1_linear_2_9"
+					x1="129"
+					y1="69"
+					x2="75.5"
+					y2="69"
+					gradientUnits="userSpaceOnUse"
+				>
+					<stop stopColor="#FFD676" />
+				</linearGradient>
+				<linearGradient
+					id="paint2_linear_2_9"
+					x1="11"
+					y1="69"
+					x2="28.5"
+					y2="69"
+					gradientUnits="userSpaceOnUse"
+				>
+					<stop stopColor="#FFD676" />
+					<stop offset="1" stopColor="#FFC846" />
+				</linearGradient>
+			</defs>
+		</svg>
+	);
+}
